264eca32d9cdba0730e51c28f1c1e7bf51e5c216
hthuz <hanggang.20@intl.zju.edu.cn>
2023-06-01 09:20:59 +0800

    Delete: pycache

--- python/django/mysite/polls/urls.py
+++ python/django/mysite/polls/urls.py
@@ -3,6 +3,23 @@ from django.urls import path
 
 from . import views
 
+# Namespace
+app_name = "polls"
+
 urlpatterns = [
+        # /polls/
         path("",views.index, name="index"),
-        ]
+
+        # /polls/5/
+        # Using angle brackets to "capture" part of url
+        # int: converter
+        # 'name' is called by {% url %} tag in template html
+        path("<int:question_id>/",views.detail,name="detail"),
+
+        # /polls/5/results/
+        path("<int:question_id>/results/",views.results, name="results"),
+
+        # /polls/5/vote/
+        path("<int:question_id>/vote/",views.vote, name="vote"),
+
+]
--- python/django/mysite/polls/views.py
+++ python/django/mysite/polls/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+
+
+from .models import Question,Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # Mapping template variable to python objects
+    context = {
+            "latest_question_list":latest_question_list,
+    }
+
+    # render(request object, tempalte name, dictionary )
+    # returns an HttpResponse object of given template rendered with
+    # the given context
+    return render(request, "polls/index.html",context)
+
+
+# When user request a page like localhost:8000/polls/5
+# input request is <HttpRequest object>, question_id is 5
+def detail(request, question_id):
+
+    # return a HttpResponse object containing content for 
+    # requested page or Http404
+    # return HttpResponse("You're looking at question %s" % question_id)
+
+    # Similar to question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question,pk = question_id)
+
+    return render(request, "polls/detail.html",{"question":question})
+
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # Note on the input of function request
+        # request.POST access submitted data (from user) by key name.
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError,Choice.DoesNotExist):
+        return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "You didn't select a choice",
+                }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        # Return a HttpResponseRedirect after dealing with POST data to prevent
+        # data being posted twice if a user hits back button
+        # Takes URL to which user will be redirected as input
+
+        # reverse return a string like /polls/3/results/
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+
