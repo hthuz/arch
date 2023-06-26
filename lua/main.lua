da8463de19d9ad2ebecb80ac65cae66e6eaeaacd
hthuz <hanggang.20@intl.zju.edu.cn>
2023-06-26 13:52:19 +0800

    Update: lua code

--- lua/main.lua
+++ lua/main.lua
@@ -1,4 +1,9 @@
 
+local function tprint(table)
+    for k,v in pairs(table) do
+        print(k,v)
+    end
+end
 
 -- Use require to run the file mod.lua
 -- Note that if you require this mod multiple times, it is only run once
@@ -12,6 +17,10 @@ for k,v in pairs(mod) do
 end
 
 
+local t = {}
+t[1] = 12
+t[2] = 34
+t[4] = 33
 
-
+tprint(t)
 
