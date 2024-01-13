e069b28c2f0708e414b03525b58d5be67053a162
hthuz <hanggang.20@intl.zju.edu.cn>
2024-01-13 21:26:59 +0800

    Add: open doc script

--- python/machine_precision.py
+++ python/machine_precision.py
@@ -3,6 +3,12 @@ import numpy as np
 
 print(np.finfo(np.float16))
 
+# half precision: 16 bits float
+# single precison: 32 bits float
+# double precison: 64 bits float
+# quad precision: 128 bits float
+# no 256 bits float now
+
 # 1 sign, 5 exponent, 10 mantissa
 # max 0 11110 1111111111 => 2^(30 - 15) * (1 + 1023 / 1024) = 65504
 # epsilon fl(1 + eps) > 1. i.e. (1 + 1 / 1024) - 1 = 1 / 1024 = 9.76e-4
