c2fb3747f1a937884651b4581d4e4e89914b19a9
hthuz <hanggang.20@intl.zju.edu.cn>
2024-05-18 11:36:38 +0800

    add: calculate in list to set

--- python/compare_time.py
+++ python/compare_time.py
@@ -8,17 +8,19 @@ list takes time O(n)
 dict, dict.keys(), set takes time O(1) and their time consumption is similar
 dict and dict.keys() are similar but it seems that dict.keys() are a bit faster ?
 np.array is also fast but not sure about time complexity
+if you try to transform a list to a set first. it doesn't reduce time
 '''
 
 def timer(func):
 
     def wrapper(*args, **kwargs):
+        times = 3
         ret_val = None
         begin = time.time()
-        for _ in range(10):
+        for _ in range(times):
             ret_val = func(*args, **kwargs)
         end = time.time()
-        print(f"func: {func.__name__}, arg:{args[0]}, times: 10, avg time: {(end - begin) / 10}")
+        print(f"func: {func.__name__}, arg:{args[0]}, times: {times}, avg time: {(end - begin) / times}")
         return ret_val
 
     return wrapper
@@ -44,12 +46,27 @@ def isInSet(item, myset: set):
 def isInArray(item, myarray):
     return item in myarray
 
+@timer
+def isInListToSet(item, mylist: list):
+    return item in set(mylist)
+
+@timer 
+def isInListToTrasformedSet(item, mylist: list):
+    myset = set(mylist)
+    return item in myset
+
 size = 10000000
 
 mylist = [i for i in range(size)]
 isInList(999, mylist)
 isInList(size - 199, mylist)
 isInList(10888888,mylist)
+isInListToSet(999, mylist)
+isInListToSet(size - 199, mylist)
+isInListToSet(10888888, mylist)
+isInListToTrasformedSet(999, mylist)
+isInListToTrasformedSet(size - 199, mylist)
+isInListToTrasformedSet(10888888, mylist)
 del mylist
 
 mydict = {k:k * 2 for k in range(size)}
